29d42e26f8efa6cb5cb1b5527ab87dc1de88d0bf
Baykov Fedor <baykov.f@gmail.com>
2020-08-11 19:52:54 +0300

    add a Routing to navigate through menu-item elements

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
 
-import HomePage from './pages/homepage/homepage.components';
+import HomePage from "./pages/homepage/homepage.components";
 
-import './App.css';
+import "./App.css";
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
 
 function App() {
   return (
-    <div className='App'>
-        <HomePage />
+    <div className="App">
+      <Route exact path="/" component={HomePage} />
+      <Route exact path="/shop/hats" component={HatsPage} />
     </div>
   );
 }
