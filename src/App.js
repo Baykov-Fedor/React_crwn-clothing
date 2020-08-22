140ac86470a7fbec7c0cc37a7ac8f656f5fb261e
Baykov Fedor <baykov.f@gmail.com>
2020-08-22 21:05:51 +0300

    Add header and form-input components. Sign page with sign-in component

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 
 import HomePage from "./pages/homepage/homepage.components";
-import ShopPage from "./pages/homepage/shop/shop-page.components";
+import ShopPage from "./pages/shop/shop-page.components";
+import Header from "./components/header/header.component";
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.components";
 
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Route exact path="/" component={HomePage} />
       <Route exact path="/shop" component={ShopPage} />
+      <Route exact path="/signin" component={SignInAndSignUpPage} />
     </div>
   );
 }
--- src/pages/shop/shop-page.components.jsx
+++ src/pages/shop/shop-page.components.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import "./shop-page.styles.scss";
 import SHOP_DATA from "./shop.data";
-import CollectionPreview from "../../../components/preview-collection/collection-preview.components";
+import CollectionPreview from "../../components/preview-collection/collection-preview.components";
 
 class ShopPage extends React.Component {
   constructor(props) {
