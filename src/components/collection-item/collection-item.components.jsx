98ebf5a1496c568210d08214802dbca1b6e79110
Baykov Fedor <baykov.f@gmail.com>
2020-08-26 17:23:10 +0300

    added cart utils to group items cart reducer, update some styles for collection-item

--- src/components/collection-item/collection-item.components.jsx
+++ src/components/collection-item/collection-item.components.jsx
@@ -1,20 +1,34 @@
 import React from "react";
+import { connect } from "react-redux";
+
+import CustomButton from "../custom-button/custom-button.components";
+import { addToCart } from "../../redux/cart/cart.actions";
 
 import "./collection-item.styles.scss";
 
-const CollectionItem = ({ id, name, imageUrl, price }) => (
-  <div className="collection-item">
-    <div
-      className="image"
-      style={{
-        backgroundImage: `url(${imageUrl})`,
-      }}
-    />
-    <div className="collection-footer">
-      <span className="name">{name}</span>
-      <span className="price">{price}</span>
+const CollectionItem = ({ item, addToCart }) => {
+  const { id, name, imageUrl, price } = item;
+  return (
+    <div className="collection-item">
+      <div
+        className="image"
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      />
+      <div className="collection-footer">
+        <span className="name">{name}</span>
+        <span className="price">{price}</span>
+      </div>
+      <CustomButton inverted onClick={() => addToCart(item)}>
+        Add to cart
+      </CustomButton>
     </div>
-  </div>
-);
+  );
+};
 
-export default CollectionItem;
+const mapDispatchToProps = (dispatch) => ({
+  addToCart: (item) => dispatch(addToCart(item)),
+});
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -3,3 +3,8 @@ const { cardActionTypes } = require("./cart.types");
 export const toogleCartHidden = () => ({
   type: cardActionTypes.TOOGLE_CART_HIDDEN,
 });
+
+export const addToCart = (item) => ({
+  type: cardActionTypes.ADD_ITEM,
+  payload: item,
+});
