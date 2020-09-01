36a54b9cf503116aa5d559fd46fd70baf2a6bad9
Baykov Fedor <baykov.f@gmail.com>
2020-09-01 12:16:26 +0300

    moved directory and shop data into its own reducers, created collection page

--- src/pages/shop/shop-page.component.jsx
+++ src/pages/shop/shop-page.component.jsx
@@ -1,13 +1,19 @@
 import React from "react";
+import { Route } from "react-router-dom";
 
 import CollectionOverview from "../../components/collection-overview/collection-overview.component";
+import CollectionPage from "../collection/collection.component";
 
 import "./shop-page.styles.scss";
 
-const ShopPage = ({ collections }) => (
-  <div className="shop-page">
-    <CollectionOverview />
-  </div>
-);
+const ShopPage = ({ match }) => {
+  console.log(match);
+  return (
+    <div className="shop-page">
+      <Route exact path={`${match.path}`} component={CollectionOverview} />
+      <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+    </div>
+  );
+};
 
 export default ShopPage;
--- src/redux/shop/shop.selector.js
+++ src/redux/shop/shop.selector.js
@@ -2,7 +2,18 @@ import { createSelector } from "reselect";
 
 const selectShop = (state) => state.shop;
 
-export const selectShopCollection = createSelector(
+export const selectCollections = createSelector(
   [selectShop],
   (shop) => shop.collections
 );
+
+export const selectCollectionsForPreview = createSelector(
+  [selectCollections],
+  (collections) => Object.keys(collections).map((keys) => collections[keys])
+);
+
+export const selectCollection = (collectionParamUrl) =>
+  createSelector(
+    [selectCollections],
+    (collections) => collections[collectionParamUrl]
+  );
